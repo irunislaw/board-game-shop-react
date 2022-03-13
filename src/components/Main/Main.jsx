1dd0fc758d3523d65aa5f17aaad5349a9b7f6881
irunislaw <84100482+irunislaw@users.noreply.github.com>
2022-03-13 20:03:20 +0100

    slider poziomy

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import "./style.css";
 const Main = () => {
   return (
-    <div id="tlo">
+    <div id="tlo1">
         <div id="slider">
             <div id="strzalka-lewo">
-<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg></div>
+<svg id="strzalka-lewo" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
+            </div>
+        <div id="strzalka-prawo">
+            
+<svg id="strzalka-prawo" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8-8-8z"/></svg>
         </div>
+</div>
         <div id="hotShot">
             <a id="hotText">Gorący strzał</a>
         </div>
-
+        
 
 
 
